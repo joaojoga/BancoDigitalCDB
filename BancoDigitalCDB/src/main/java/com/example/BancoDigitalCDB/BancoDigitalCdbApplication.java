4a070a491f6d689122465d0d1c2aa83cdd1368dd
joaojoga <joaovictormontanaro@gmail.com>
2025-06-24 14:29:08 -0300

    Adicionando o projeto com spring

---
+++ BancoDigitalCDB/src/main/java/com/example/BancoDigitalCDB/BancoDigitalCdbApplication.java
@@ -0,0 +1,13 @@
+package com.example.BancoDigitalCDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDigitalCdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDigitalCdbApplication.class, args);
+	}
+
+}
